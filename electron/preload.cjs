d9523dd7c36fe30303b99b3f224284b94a106dba
DaemonCore.app <gtited@gmail.com>
2026-08-29 03:45:25 -0400

    Call this one 5.2

--- electron/preload.cjs
+++ electron/preload.cjs
@@ -2,7 +2,7 @@ const { contextBridge, ipcRenderer } = require('electron')
 
 contextBridge.exposeInMainWorld('daemoncore', {
   platform: process.platform,
-  version: '5.1.0',
+  version: '5.2.0',
   display: Object.freeze({
     setZoom: factor => ipcRenderer.invoke('display:set-zoom', factor),
   }),
