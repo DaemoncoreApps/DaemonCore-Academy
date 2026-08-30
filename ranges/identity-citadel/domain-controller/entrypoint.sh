09ebe93ba2721d90fcaeb0c3961b1dc113b655c6
DaemonCore.app <gtited@gmail.com>
2026-08-29 23:53:05 -0400

    Give LDAP its Kerberos identity

--- ranges/identity-citadel/domain-controller/entrypoint.sh
+++ ranges/identity-citadel/domain-controller/entrypoint.sh
@@ -6,5 +6,6 @@ if [ ! -f /state/etc/smb.conf ]; then
   samba-tool user create svc.backup 'Service-Lab-42!' -s /state/etc/smb.conf
   samba-tool group add 'Backup Operators Lab' -s /state/etc/smb.conf
   samba-tool group addmembers 'Backup Operators Lab' svc.backup -s /state/etc/smb.conf
+  samba-tool spn add ldap/dc01.daemoncore.lab 'DC01$' -s /state/etc/smb.conf
 fi
 exec samba -i --debug-stdout -s /state/etc/smb.conf
