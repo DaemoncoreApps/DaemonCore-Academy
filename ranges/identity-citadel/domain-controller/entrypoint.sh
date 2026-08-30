12d8f3cbb46ac8f0a983b4ec6f3930eb1bf8611d
DaemonCore.app <gtited@gmail.com>
2026-08-29 23:44:10 -0400

    Let the identity range build where it runs

--- ranges/identity-citadel/domain-controller/entrypoint.sh
+++ ranges/identity-citadel/domain-controller/entrypoint.sh
@@ -1,4 +1,10 @@
 #!/bin/sh
 set -eu
-cp -a /opt/citadel-seed/. /state/
+if [ ! -f /state/etc/smb.conf ]; then
+  samba-tool domain provision --realm=DAEMONCORE.LAB --domain=DAEMONCORE --server-role=dc --dns-backend=SAMBA_INTERNAL --adminpass='Citadel-Lab-42!' --use-rfc2307 --targetdir=/state
+  samba-tool -s /state/etc/smb.conf user create lab.operator 'Operator-Lab-42!'
+  samba-tool -s /state/etc/smb.conf user create svc.backup 'Service-Lab-42!'
+  samba-tool -s /state/etc/smb.conf group add 'Backup Operators Lab'
+  samba-tool -s /state/etc/smb.conf group addmembers 'Backup Operators Lab' svc.backup
+fi
 exec samba -i --debug-stdout -s /state/etc/smb.conf
